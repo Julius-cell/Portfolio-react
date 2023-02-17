34d533b40c387860ba6ceb88c4c5a5a0a562f83b
Julio Cid <julio.cid.b@gmail.com>
2023-02-16 22:20:42 -0300

    add login page

--- src/pages/login-page/login-page.jsx
+++ src/pages/login-page/login-page.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
+
+  const navigation = useNavigate()
+
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleChange = (e, field) => {
+    setUser({
+      ...user,
+      [field]: e.target.value
+    })
+  }
+
+  const submit = (e) => {
+    e.preventDefault()
+    console.log(user);
+    navigation("/")
+  }
+
   return (
-    <div>login-page</div>
+    <div>
+      <h1>Iniciar sesión</h1>
+
+      <form onSubmit={submit}>
+        <div> 
+          <label htmlFor="email">Correo</label>
+          <input onChange={e => handleChange(e, 'email')} type="email" name='email' />
+        </div>  
+        <div>
+          <label htmlFor="password">Password</label>
+          <input onChange={e => handleChange(e, 'password')} type="password" name='password' />
+        </div>  
+        <div>
+          <input type="submit" vale='Ingresar' />
+        </div>  
+      </form>
+    </div>
   )
 }
 
